c31870b0f30cd9fcdf54f264ceef7452feb89169
Siavash Samadi <shsf1382@gmail.com>
2022-07-17 22:29:16 +0430

    Outputting UserList Component ✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/AddUser/AddUser";
+import UserList from "./components/UserList/UserList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
     </div>
   );
 }

f5f70efcf0ecdaff16b3737c68cf158fef72432c
Siavash Samadi <shsf1382@gmail.com>
2022-07-20 00:16:08 +0430

    Adding Fragment (React built-in wrapper) ➕

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import AddUser from "./components/AddUser/AddUser";
 import UserList from "./components/UserList/UserList";
 
@@ -15,10 +15,10 @@ function App() {
   };
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UserList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
